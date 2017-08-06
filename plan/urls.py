3c1f9a4958e623aa64074fd28529b2a83d97a06d
lazydancer <jrpucula@gmail.com>
2017-08-06 16:08:26 -0400

    Experimenting

--- plan/urls.py
+++ plan/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^ajax/test/$', views.ajax_test, name='ajax_test'),
 ]
--- plan/views.py
+++ plan/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.http import HttpResponse
 from .models import Recipe, Ingredient
 
 from plan.conversion import combine, friendlyUnits
@@ -18,7 +21,6 @@ def groupbyDepartment(ingredient_list):
 		groupedIngredientList.append(group)
 
 	return groupedIngredientList
-
 
 def index(request):
 	latest_recipe_list = Recipe.objects.order_by('id')[:6]
@@ -43,3 +45,22 @@ def index(request):
 class DetailView(generic.DetailView):
 	model = Recipe
 	template_name = 'plan/detail.html'
+
+def ajax_test(request):
+    if request.method == 'POST':
+        post_text = request.POST.get('the_post')
+        response_data = {}
+        
+
+        response_data['result'] = 'Create post successful!'
+        response_data['post_text'] = post_text
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
