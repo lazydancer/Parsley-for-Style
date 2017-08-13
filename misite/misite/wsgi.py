7bb74befa398a20e3eda36dc69d75184f6ec7efc
lazydancer <jrpucula@gmail.com>
2017-08-13 17:59:25 -0400

    mor things

--- misite/misite/wsgi.py
+++ misite/misite/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "misite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "misite.settings")
+
 
 application = get_wsgi_application()
