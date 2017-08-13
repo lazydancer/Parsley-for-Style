a4ab98f32be40c0128bc16689f39794adb666b32
lazydancer <jrpucula@gmail.com>
2017-08-13 14:25:17 -0400

    updated settings

--- misite/misite/settings.py
+++ misite/misite/settings.py
@@ -74,6 +74,18 @@ WSGI_APPLICATION = 'misite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
