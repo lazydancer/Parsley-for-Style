77f912cd3fdeac8b3b66ca2a5c34bb019de44808
lazydancer <jrpucula@gmail.com>
2017-08-12 20:02:46 -0400

    removal of polls

--- misite/settings.py
+++ misite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'plan.apps.PlanConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
