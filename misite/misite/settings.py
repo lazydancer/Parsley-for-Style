08b64d3cfee7cd2543bbe83f61634d9e9c4e2d3f
lazydancer <jrpucula@gmail.com>
2017-09-25 21:19:28 -0400

    Updating to Postgres database

--- misite/misite/settings.py
+++ misite/misite/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "plan", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "misite", "plan", "static")
 STATIC_URL = '/plan/static/'
