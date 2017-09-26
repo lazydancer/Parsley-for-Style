18225bc66576b09b55b2221a414a3d74adbe9448
lazydancer <jrpucula@gmail.com>
2017-09-25 21:14:04 -0400

    Addition of admin user

--- misite/misite/settings.py
+++ misite/misite/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "plan", "static")
+STATIC_URL = '/plan/static/'
